6ccef2aecc119118800171129069348dda9e2678
Jared Asuncion <guissmo@gmail.com>
2023-10-29 09:15:19 +0100

    changed link to blog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <h1 id="logo">
-      <a href={`${import.meta.env.BASE_URL}/`}>GUISSMO</a>
+      <a href={`${import.meta.env.BASE_URL}/blog`}>GUISSMO</a>
     </h1>
   );
 }
