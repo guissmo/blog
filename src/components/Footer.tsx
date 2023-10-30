9a99dcef8eef2e8c7dbc0636717ec84a4e13ed72
Jared Asuncion <guissmo@gmail.com>
2023-10-30 23:49:02 +0100

    add contact details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,8 @@
 export default function Footer() {
-  return <div style={{ height: 200, paddingTop: 50 }}><a href="#">Back to Top</a> | <a href={`${import.meta.env.BASE_URL}/rss.xml`}>RSS Feed</a></div>;
+  return (
+    <div style={{ height: 200, paddingTop: 50 }}>
+      <a href="#">Back to Top</a> |{" "}
+      <a href={`${import.meta.env.BASE_URL}/rss.xml`}>Blog RSS Feed</a>
+    </div>
+  );
 }
