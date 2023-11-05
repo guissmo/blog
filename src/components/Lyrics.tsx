14e47809a1b14d5de23bf1ef5738f350d164e81e
Jared Asuncion <guissmo@gmail.com>
2023-11-05 17:35:18 +0100

    final polish

--- src/components/Lyrics.tsx
+++ src/components/Lyrics.tsx
@@ -2,20 +2,49 @@ import { useEffect, useState } from "react";
 import "./lola.css";
 
 function FourLetterWord({ word }: { word: string }) {
-  const safe = word.replace(/[^a-zA-Z]/g, "").padEnd(4, " ");
+  const safe = word.replace(/[^a-zA-Z]/g, "").padEnd(4, ".");
+  let classNameStr = "letter-box";
+  if (safe === "PLAY") classNameStr = "action";
+  if (safe !== "PLAY" && safe !== "REDO" && safe !== "WAIT") {
+    //@ts-ignore
+    document.title = safe.toUpperCase();
+  } else {
+    //@ts-ignore
+    document.title = "Lola";
+  }
   return (
     <div className="word">
-      <span className="letter-box">
-        <span class="letter">{safe.charAt(0)}</span>
+      <span className={classNameStr}>
+        <span
+          className={"letter"}
+          style={{ opacity: safe.charAt(0) == "." ? 0 : 1 }}
+        >
+          {safe.charAt(0)}
+        </span>
       </span>
-      <span className="letter-box">
-        <span class="letter">{safe.charAt(1)}</span>
+      <span className={classNameStr}>
+        <span
+          className={"letter"}
+          style={{ opacity: safe.charAt(1) == "." ? 0 : 1 }}
+        >
+          {safe.charAt(1)}
+        </span>
       </span>
-      <span className="letter-box">
-        <span class="letter">{safe.charAt(2)}</span>
+      <span className={classNameStr}>
+        <span
+          className={"letter"}
+          style={{ opacity: safe.charAt(2) == "." ? 0 : 1 }}
+        >
+          {safe.charAt(2)}
+        </span>
       </span>
-      <span className="letter-box">
-        <span class="letter">{safe.charAt(3)}</span>
+      <span className={classNameStr}>
+        <span
+          className={"letter"}
+          style={{ opacity: safe.charAt(3) == "." ? 0 : 1 }}
+        >
+          {safe.charAt(3)}
+        </span>
       </span>
     </div>
   );
@@ -41,6 +70,8 @@ export default function Lyrics({
   const [time, setTime] = useState(0);
   const [lineIndex, setLineIndex] = useState(0);
 
+  const [word, setWord] = useState("PLAY");
+
   useEffect(() => {
     const handleTimeChange = () => {
       //@ts-ignore
@@ -57,16 +88,44 @@ export default function Lyrics({
   }, []);
 
   useEffect(() => {
-    console.log(lineIndex);
+    if (time > 86) {
+      //@ts-ignore
+      window.player.pause();
+      setWord("REDO");
+      return;
+    }
+    if (lines[lineIndex] === undefined) return;
     const { start, text } = processLine(lines[lineIndex]);
+    if (lines[lineIndex - 1] !== undefined)
+      setWord(processLine(lines[lineIndex - 1]).text);
     if (start < time) setLineIndex(lineIndex + 1);
   }, [time]);
 
   return (
-    <div>
-      <FourLetterWord
-        word={lineIndex < 1 ? "" : processLine(lines[lineIndex - 1]).text}
-      />
+    <div
+      onClick={
+        word == "PLAY"
+          ? () => {
+              setWord("WAIT");
+              //@ts-ignore
+              window.player.play();
+            }
+          : word == "REDO"
+          ? () => {
+              //@ts-ignore
+              window.player.currentTime = 0;
+              setWord("PLAY");
+              setTime(0);
+              setLineIndex(0);
+            }
+          : () => {}
+      }
+      style={{
+        cursor: lineIndex < 1 || word == "REDO" ? "pointer" : "unset",
+        pointerEvents: lineIndex < 1 || word == "REDO" ? "auto" : "none",
+      }}
+    >
+      <FourLetterWord word={word} />
     </div>
   );
 }
