7207b5f07a4d921c62aedfebb498d373197cd4c5
Jared Asuncion <guissmo@gmail.com>
2024-09-21 15:31:10 +0200

    add preliminary tags page

--- src/content/blog/primecert-v2-0.md
+++ src/content/blog/primecert-v2-0.md
@@ -12,7 +12,8 @@ categories:
 description: So... I used my train rides and carshares to work on my latest personal project.
 slug: primecert-v2-0
 tags:
-  - travel
+  - primes
+  - PARI
 ---
 
 In this blog post, I talk about the journey of how I made my first FastAPI / Postgres / Svelte project incrementally as something to keep me busy in my numerous train, bus, and carshares these past few months. It felt like a dev's version of [JetLag: The Game](https://www.youtube.com/c/jetlagthegame) to be honest.
--- src/pages/blog/blog.ts
+++ src/pages/blog/blog.ts
@@ -8,19 +8,38 @@ export const allBlogPostsSortedByDate = allBlogPostsUnsorted.sort(
   ({ data: data1 }, { data: data2 }) =>
     data2.date.getTime() - data1.date.getTime()
 );
+
 export const blogPostsByCategory = function blogPostsByCategory(
   posts: CollectionEntry<"blog">[]
 ) {
   const dic = {} as { [key: string]: CollectionEntry<"blog">[] };
   posts.forEach((post) => {
     const categoryName =
-      post.data.category === undefined ? "No category" : post.data.category;
+      post.data.category === undefined ? "Uncategorized" : post.data.category;
     if (!dic.hasOwnProperty(categoryName))
       dic[categoryName] = [] as CollectionEntry<"blog">[];
     dic[categoryName].push(post);
   });
   return dic;
 };
+
+export const blogPostsByTag = function blogPostsByTag(
+  posts: CollectionEntry<"blog">[]
+) {
+  const dic = {} as { [key: string]: CollectionEntry<"blog">[] };
+  posts.forEach((post) => {
+    // if post has the tag tag return it
+    if (post.data.tags == undefined) return dic;
+    post.data.tags.forEach((tag) => {
+      if (!dic.hasOwnProperty(tag)) dic[tag] = [] as CollectionEntry<"blog">[];
+      dic[tag].push(post);
+    });
+  });
+  return dic;
+};
+
 export const allBlogPostsByCategory = blogPostsByCategory(
   allBlogPostsSortedByDate
 );
+
+export const allBlogPostsByTag = blogPostsByTag(allBlogPostsSortedByDate);
